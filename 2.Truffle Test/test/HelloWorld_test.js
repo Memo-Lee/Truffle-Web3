fb78ed5a579d4ea329e402a02dbe0403402f103b
Mehmet Sönmez <101952360+Memo-Lee@users.noreply.github.com>
2022-10-02 23:33:18 +0300

    Add files via upload

---
+++ 2.Truffle Test/test/HelloWorld_test.js
@@ -0,0 +1,19 @@
+const HelloWorld = artifacts.require("HelloWorld");
+
+contract("HelloWorld",accounts => {
+    // kontrol mekanizması için 'it' kullanılır.
+    // 1. kontrol mekanizması (First control mechanism)
+    it("consrructor should set the message correctly", async() =>{
+        let instance = await HelloWorld.deployed();
+        let message = await instance.message();
+        // Hello_World_Migrations'a eşit olmalı
+        assert.equal(message,"Hello world this is second contract text");
+    })
+    // 2. kontrol mekanizması (Second control mechanism)
+    it("owner should be accounts[0]", async() =>{
+        let instance = await HelloWorld.deployed();
+        let owner = await instance.owner();
+        // Hello_World_Migrations'a eşit olmalı
+        assert.equal(owner,accounts[0]);
+    })
+})
